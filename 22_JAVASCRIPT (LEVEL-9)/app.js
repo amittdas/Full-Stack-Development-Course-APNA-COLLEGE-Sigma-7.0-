43dcaed0169b6c762b42c59bacce7dc726bed02d
amittdas <amitdas123456789@gmail.com>
2025-08-23 01:55:00 +0530

    Added JAVASCRIPT-9

---
+++ 22_JAVASCRIPT (LEVEL-9)/app.js
@@ -0,0 +1,8 @@
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("div a"));
+
+// let img = document.querySelector('img')
+// console.dir(img.getAttribute('id'))
+// console.dir(img.getAttribute('id', 'spiderManImg'))
