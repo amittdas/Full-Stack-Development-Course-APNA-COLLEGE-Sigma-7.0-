d5ee10dc83fb2c67a6d09367ba0c86168a88af11
amittdas <amitdas123456789@gmail.com>
2026-04-14 00:29:51 +0530

    Added PROCESS

---
+++ 30_BACKEND-1 (NODE.JS)/01_process.js
@@ -0,0 +1,11 @@
+
+// PROCESS ------------------------------------------
+
+let args = process.argv;
+
+for(let i=2; i<args.length; i++){
+    console.log(args[i]);
+}
+
+// How to run js file with node js in terminal
+// -->   node <file name>
