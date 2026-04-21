65ff1485ff711a4c647f32058b658fd9efde227b
amittdas <amitdas123456789@gmail.com>
2026-04-21 22:18:19 +0530

    Added ENTRY POINT

---
+++ 32_BACKEND-3 (NODE.EJS)/EJSdir/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 8080;
+
+app.set("view engine", "ejs") ;
+app.set("views", path.join(__dirname, "/views")) ;
+
+app.get("/", (req,res) => {
+    res.render("home.ejs");
+})
+
+app.get("/hello", (req,res) => {
+    res.send("HELLOOO");
+})
+
+app.get("/rollDice", (req,res) => {
+    let diceVal = Math.floor(Math.random()*6)+1 ;
+    res.render("rollDice.ejs", {diceVal});
+})
+
+app.listen(port, () => {
+    console.log(`App is litsening on port ${port}`)
+})
